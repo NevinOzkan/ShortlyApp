1df8ce0b3264cff0ab24466876730c00bf4a5733
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-23 20:23:24 +0300

    Created HistoryVC

--- ShortlyApp/Features/View/Cell/ShortLinkCell.swift
+++ ShortlyApp/Features/View/Cell/ShortLinkCell.swift
@@ -23,9 +23,9 @@ class ShortLinkCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
+    
     @IBAction func copyButton(_ sender: Any) {
     }
     
---
+++ ShortlyApp/Features/View/HistoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryVC.swift
+//  ShortlyApp
+//
+//  Created by Nevin Özkan on 23.12.2024.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
