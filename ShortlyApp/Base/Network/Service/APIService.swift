14d1049ac04fbbf8017934f34265216da434581d
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-24 15:38:55 +0300

    APIService connected

---
+++ ShortlyApp/Base/Network/Service/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  ShortlyApp
+//
+//  Created by Nevin Özkan on 23.12.2024.
+//
+
+import Foundation
---
+++ ShortlyApp/Features/ViewModel/LinkListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LinkListViewModel.swift
+//  ShortlyApp
+//
+//  Created by Nevin Özkan on 23.12.2024.
+//
+
+import Foundation
