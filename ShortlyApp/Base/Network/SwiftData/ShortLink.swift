38dae97762a0f025c1a8447c1213fec145389f83
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-26 10:57:29 +0300

    SwiftData saveLink

---
+++ ShortlyApp/Base/Network/SwiftData/ShortLink.swift
@@ -0,0 +1,8 @@
+//
+//  ShortLink.swift
+//  ShortlyApp
+//
+//  Created by Nevin Özkan on 24.12.2024.
+//
+
+import Foundation
---
+++ ShortlyApp/Test/MockAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  MockAPIService.swift
+//  ShortlyApp
+//
+//  Created by Nevin Özkan on 25.12.2024.
+//
+
+import Foundation
